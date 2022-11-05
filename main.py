9b629617b7576d5448428385b14b2d765f5f3677
Ayush V. Patel <81207867+Ayush253@users.noreply.github.com>
2022-11-05 11:52:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,14 +32,14 @@ while game_is_on:
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
 
-    if ball.distance(right_paddle) < 50 and ball.xcor() > 320 or ball.distance(left_paddle) < 50 and ball.xcor() < -400:
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 320 or ball.distance(left_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
     if ball.xcor() > 380:
         ball.reset_position()
         scoreboard.l_point()
 
-    if ball.xcor() < -330:
+    if ball.xcor() < -380:
         ball.reset_position()
         scoreboard.r_point()
 
